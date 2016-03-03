046828878186ad1ec538b5130a92170deb8d3096
medion chou <medion.dc@gmail.com>
2016-03-03 19:43:04 +0800

    Chang Command class for the simplicity of
    changing device name.

--- app/src/main/java/com/example/medionchou/tobacco/Constants/Command.java
+++ app/src/main/java/com/example/medionchou/tobacco/Constants/Command.java
@@ -5,7 +5,7 @@ package com.example.medionchou.tobacco.Constants;
  */
 public class Command {
     public final static String QUERY_ONLINE_STATE = "QUERY\tONLINE_STATE<END>";
-    public final static String ID = "PMM";
+    public final static String ID = "MM";
     public final static String CONNECT_SERVER = "CONNECT\t" + ID + "<END>";
     public final static String WH_NOW_THREE = "QUERY\tWH_NOW\t3\t";
     public final static String WH_NOW_FIVE = "QUERY\tWH_NOW\t5\t";
